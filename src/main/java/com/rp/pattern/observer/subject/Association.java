13a6b8f4cd81f41bc7271902672c48d0da113138
vincentmylee <vincentmylee@gmail.com>
2019-09-26 16:45:20 +0800

    Subject class not need type, " T "

--- src/main/java/com/rp/pattern/observer/subject/Association.java
+++ src/main/java/com/rp/pattern/observer/subject/Association.java
@@ -1,7 +1,5 @@
 package com.rp.pattern.observer.subject;
 
-import com.rp.pattern.observer.adventurer.Adventurer;
-
 public class Association extends Subject{
     public void sendQuestions(final String questions) {
         list.stream().forEach(adventurer ->  adventurer.getQuestions(questions));
